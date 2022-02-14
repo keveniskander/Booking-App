9aa5c353cfd85dc7da65119f7ce9557ac8682bd9
keveniskander <keven.iskander@hotmail.com>
2022-02-14 11:03:40 -0500

    array practice

--- main.go
+++ main.go
@@ -6,15 +6,17 @@ func main() {
 
 	conferenceName := "Go Conference"
 
-	// const conferenceTickets uint = 50
+	const conferenceTickets int = 50
 
 	var remainingTickets uint = 50
 
-	// fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	// fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var bookings [50]string
 
 	var firstName string
 	var lastName string
@@ -39,6 +41,12 @@ func main() {
 	// userTickets = 2
 
 	remainingTickets = remainingTickets - userTickets
+	bookings[0] = firstName + " " + lastName
+
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Array type: %T\n", bookings)
+	fmt.Printf("Array length: %v\n", len(bookings))
 
 	// fmt.Printf("User %v booked %v tickets.\n", firstName, userTickets)
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
