3b0e72426e745a8562d07491fd1e68d2a6673c40
keveniskander <keven.iskander@hotmail.com>
2022-02-14 10:49:56 -0500

    pointer and scan user input practice

--- main.go
+++ main.go
@@ -10,10 +10,10 @@ func main() {
 
 	var remainingTickets uint = 50
 
-	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+	// fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	// fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
 	var firstName string
