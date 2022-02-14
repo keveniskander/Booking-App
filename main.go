26da632fda78e0411292929fc1fc9d582034127a
keveniskander <keven.iskander@hotmail.com>
2022-02-14 09:54:41 -0500

    variables

--- main.go
+++ main.go
@@ -6,7 +6,20 @@ func main() {
 
 	var conferenceName = "Go Conference"
 
-	fmt.Println("Welcome to", conferenceName, "booking application")
+	const conferenceTickets = 50
+
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+
+	userName = "Tom"
+	userTickets = 2
+
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 
 }
